13610047929e7bc702e0ff8a556643feb0f5b2af
Oliver Schemmel <olis@MacBook.local>
2008-10-17 23:36:39 +0200

    default article, if there isn't any yet

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @article = {:title => I18n.t('txt.article.none.title'), :body => I18n.t('txt.article.none.body')}
+  end
+  
 end
